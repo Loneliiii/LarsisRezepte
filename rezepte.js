a39cdc4f203b93a3f00bb1d4f1e3e028d7fda8bc
Loneliiii <141322052+Loneliiii@users.noreply.github.com>
2026-02-21 16:54:50 +0100

    Update image path for Hähnchen Süß-Sauer recipe

--- rezepte.js
+++ rezepte.js
@@ -105,7 +105,7 @@ const rezeptDaten = {
 
     "huhn_suess_sauer": {
         titel: "Nudeln mit Hähnchen Süß-Sauer-Sahne Sauce",
-        bild: "Bilder/nudels_süß_sauer.jpg",
+        bild: "Bilder/nudelnsusa.jpg",
         zeit: "25 Min.",
         portionen: "2 Portionen",
         kategorie: "Hauptgericht, Schnell",
